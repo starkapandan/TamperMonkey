4f901a35526a1bffc132916ad14722d05af58826
starkapandan <gabriel.aslaner@gmail.com>
2020-02-23 15:12:22 +0100

    hej

--- vkMaxQuality.js
+++ vkMaxQuality.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Vk Max Video Quality
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  try to take over the world!
 // @author       You
 // @include      http*://*vk.com/*
