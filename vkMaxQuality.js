48a331ef59b424f2401f6bdfe32947b3fb091f3e
Gabriel Aslaner <gabriel.aslaner@gmail.com>
2020-03-02 16:10:04 +0100

    s

--- vkMaxQuality.js
+++ vkMaxQuality.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Vk Max Video Quality
 // @namespace    http://tampermonkey.net/
-// @version      1.1
+// @version      2.0
 // @description  try to take over the world!
 // @author       You
 // @include      http*://*vk.com/*
@@ -9,6 +9,7 @@
 // ==/UserScript==
 
 var lastDocument = "";
+var lastQuality = 0;
 
 function SetVKMaxQuality() {
     var settingsBtn = document.getElementsByClassName("videoplayer_btn_settings")[0];
@@ -23,11 +24,10 @@ function SetVKMaxQuality() {
     qualityBtn.click();
     var qualityDivs = document.getElementsByClassName("videoplayer_settings_menu_sublist_item");
 
-
     var MaxQualityDiv = undefined;
     var MaxQuality = 0;
     for (var i = 0; i < qualityDivs.length; i++) {
-        var quality = parseInt(qualityDivs[i].dataset["value"])
+        var quality = parseInt(qualityDivs[i].dataset.value)
         if (quality > MaxQuality) {
             MaxQualityDiv = qualityDivs[i];
             MaxQuality = quality;
@@ -37,27 +37,30 @@ function SetVKMaxQuality() {
         return 0;
     }
     MaxQualityDiv.click();
+    lastQuality = MaxQuality;
     return 1;
 }
 
 async function ListenToDocumentChanges() {
-    await new Promise(r => setTimeout(r, 1500));
     while (true) {
-        console.log("<Iteration>")
-        if (lastDocument != document.location) {
-            lastDocument = document.location;
+        if (lastDocument != document.location.href) {
+            lastDocument = document.location.href;
             var status = SetVKMaxQuality();
             if (status == 1) {
-                console.log("Set Quality!")
-                break;
+                if(lastQuality == 1080){
+                    break;
+                }
             }
         }
         await new Promise(r => setTimeout(r, 5000));
     }
-    console.log("END")
 }
 
 (function () {
     'use strict';
-    ListenToDocumentChanges();
+    if (window.top != window.self)
+        return;
+    window.onload = function () {
+        ListenToDocumentChanges();
+    };
 })();
