fd190d3a4dde4492b1770609ca67694807abb3c0
Gabriel <Gabriel@MacBook-Pro-GA.local>
2020-09-29 23:12:43 +0200

    d

---
+++ viafree.js
@@ -0,0 +1,35 @@
+// ==UserScript==
+// @name         HLS video Getter
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @include        http*://*viafree.se/*
+// @grant        none
+// ==/UserScript==
+
+function findByKey(obj, key) {
+    if(obj==null || obj==undefined ){
+        return;
+    }
+    if(obj.constructor === Object || obj.constructor === Array){
+        var keys = Object.keys(obj);
+        for(i in keys){
+            var i = keys[i];
+            if(key == i){
+                return obj; 
+            }else{
+                var test = findByKey(obj[i], key);
+                if(test != undefined){
+                    return test;
+                }
+            }
+        }
+    }else if(obj == key){
+        return key;
+    }
+    return;
+
+}
+
+findByKey(window.__initialState__, "adBlockerBlocker")['adBlockerBlocker']['enabled'] = false;
