3b0939b4beb6472b8a6ef6f525305acd8ee3f22a
Gabriel Aslaner <gabriel.aslaner@gmail.com>
2021-01-03 20:05:23 +0100

    asd

--- imgCrawler.js
+++ imgCrawler.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         image Crawler
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  try to take over the world!
 // @author       You
 // @include      *://*/*
