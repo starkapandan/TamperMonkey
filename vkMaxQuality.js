336af78f56773c71605555100514fd0d57562f7e
Gabriel Aslaner <gabriel.aslaner@gmail.com>
2021-01-03 17:52:36 +0100

    asd

--- vkMaxQuality.js
+++ vkMaxQuality.js
@@ -1,7 +1,7 @@
 // ==UserScript==
-// @name         imgCrawler
+// @name         Vk Max Video Quality
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      2.1
 // @description  try to take over the world!
 // @author       You
 // @include      http*://*vk.com/*
