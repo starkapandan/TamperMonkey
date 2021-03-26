09545464e010f371d97e733973c3d02808cec72f
Gabriel Aslaner <gabriel.aslaner@gmail.com>
2021-03-26 22:05:23 +0100

    k

--- HLS video Getter.js
+++ HLS video Getter.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         HLS video Getter
 // @namespace    http://tampermonkey.net/
-// @version      0.3
+// @version      0.4
 // @description  try to take over the world!
 // @author       You
 // @include        http*://*kanald.com.tr/*
