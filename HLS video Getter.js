3ec110c4e2209223369364b84b9ff844a686931f
Gabriel Aslaner <gabriel.aslaner@gmail.com>
2021-04-27 22:52:43 +0200

    das

--- HLS video Getter.js
+++ HLS video Getter.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         HLS video Getter
 // @namespace    http://tampermonkey.net/
-// @version      0.5
+// @version      0.6
 // @description  try to take over the world!
 // @author       You
 // @include        http*://*kanald.com.tr/*
@@ -11,28 +11,28 @@
 // ==/UserScript==
 
 
-//host, just check if matches current website
-//request, check if matches AND returned the matched link!
+//host, just check if matches current browser url of window.location.hostname
+//request, check if matches AND returned the captured part of the link!
 var LinkSearchPattern = [
     {
-        host: "kanald.com.tr",
+        host: /kanald.com.tr/,
         request: [
-            { pattern: ".*1000/prog_index.m3u8.*",}
+        { pattern: /.*1000\/prog_index.m3u8.*/}
         ],
     }, {
-        host: "discoveryplus.se",
+        host: /discoveryplus.se/,
         request: [
-            { pattern: ".*playlist.m3u8.*", },
+            { pattern: /.*playlist.m3u8.*/, },
         ],
     }, {
-        host: "vk.com",
+        host: /vk.com/,
         request: [
-            { pattern: ".*mycdn.*type=5.*&id=.*&", title: "1080p" },
-            { pattern: ".*mycdn.*type=3.*&id=.*&", title: "720p" },
-            { pattern: ".*mycdn.*type=2.*&id=.*&", title: "480p" },
-            { pattern: ".*mycdn.*type=1.*&id=.*&", title: "360p" },
-            { pattern: ".*mycdn.*expires=.*&id=.*&", title: "others" },
-            { pattern: ".*hls.*\.m3u8", title: "m3u8 file" },
+            { pattern: /.*mycdn.*type=5.*&id=.*&/, title: "1080p" },
+            { pattern: /.*mycdn.*type=3.*&id=.*&/, title: "720p" },
+            { pattern: /.*mycdn.*type=2.*&id=.*&/, title: "480p" },
+            { pattern: /.*mycdn.*type=1.*&id=.*&/, title: "360p" },
+            { pattern: /.*mycdn.*expires=.*&id=.*&/, title: "others" },
+            { pattern: /.*hls.*\.m3u8/, title: "m3u8 file" },
         ],
     },
 ];
@@ -43,25 +43,14 @@ function addXMLRequestCallback(callback) {
     var oldSend, i;
     if (XMLHttpRequest.callbacks) {
         var x = $("something appearance")
-        // we've already overridden send() so just add the callback
         XMLHttpRequest.callbacks.push(callback);
     } else {
-        // create a callback queue
         XMLHttpRequest.callbacks = [callback];
-        // store the native send()
         oldSend = XMLHttpRequest.prototype.send;
-        // override the native send()
         XMLHttpRequest.prototype.send = function () {
-            // process the callback queue
-            // the xhr instance is passed into each callback but seems pretty useless
-            // you can't tell what its destination is or call abort() without an error
-            // so only really good for logging that a request has happened
-            // I could be wrong, I hope so...
-            // EDIT: I suppose you could override the onreadystatechange handler though
             for (i = 0; i < XMLHttpRequest.callbacks.length; i++) {
                 XMLHttpRequest.callbacks[i](this);
             }
-            // call the native send()
             oldSend.apply(this, arguments);
         }
     }
@@ -84,7 +73,7 @@ function seekPattern(link) {
     for (var i = 0; i < activeHostPatterns.length; i++) {
         var requestPattern = activeHostPatterns[i].pattern;
         var matches = link.match(requestPattern);
-        if (matches != undefined) {
+        if (matches) {
             var returnPack = {matched: matches[0], title: ""};
             if("title" in activeHostPatterns[i]){
                 returnPack.title = activeHostPatterns[i].title;
@@ -114,7 +103,7 @@ function CheckLinkForMatch(link) {
 
     for (var i = 0; i < LinkSearchPattern.length; i++) {
         var hostPattern = LinkSearchPattern[i].host;
-        if (window.location.href.match(hostPattern) != undefined) {
+        if (window.location.hostname.match(hostPattern) != undefined) {
             activeHostPatterns = LinkSearchPattern[i].request;
             break;
         }
